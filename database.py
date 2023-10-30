7a5ba95ff8008ff17bf116468d62b7ea8d2c4083
gogogagarin <33862385+gogogagarin@users.noreply.github.com>
2023-10-30 22:52:02 +0000

    rettet database info

--- database.py
+++ database.py
@@ -1,20 +1,18 @@
 from sqlalchemy import create_engine, text
+import os
+import mysql.connector
 
-db_connection_string =
-os.environ['DB_CONNECTION_STRING']
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(
   db_connection_string, 
   echo=True)
 
 # PyMySQL
-def load_vare_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM case22.Vare"))
-    print(result.fetchall())
-    vare = []
-    for row in result.all():
-      vare.append(dict(row))
-    return vare
+with engine.connect() as conn:
+  result = conn.execute(text("SELECT * FROM case22.Vare"))
+  print(result.fetchall())
+    
 
 
