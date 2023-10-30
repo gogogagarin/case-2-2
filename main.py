8e4d3b872827e7cef22086ab8b315b27d84d0de6
gogogagarin <33862385+gogogagarin@users.noreply.github.com>
2023-10-30 15:49:00 +0000

    added pages

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_jovian():
+  return render_template('home.html')
+
+@app.route("/vare")
+def vare():
+  return render_template('vare.html')
+  
+@app.route("/ordre")
+def ordre():
+  return render_template('ordre.html')
+  
+@app.route("/faktura")
+def faktura():
+  return render_template('faktura.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+  
