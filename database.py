5353eddc9c41622fe0adc838f0fdb9bf663c3576
gogogagarin <33862385+gogogagarin@users.noreply.github.com>
2023-11-09 16:25:33 +0000

    jan 17.24

--- database.py
+++ database.py
@@ -1,15 +1,22 @@
+# Import the necessary modules
 import mysql.connector
 import os
 
-
+# Define a function to execute a query
 def execute_query(text):
+  # Connect to the database
   db = mysql.connector.connect(host='gateway01.us-west-2.prod.aws.tidbcloud.com',
                                port='4000',
                                user='3aHUY11sUPdpx1P.root',
                                password=os.environ['PW'],
                                database='gruppe3_nemlig')
+  # Create a cursor
   cur = db.cursor()
+  # Execute the query
   cur.execute(text)
+  # Fetch all the results
   data = cur.fetchall()
+  # Close the cursor
   cur.close()
+  # Return the data
   return data
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+# Import the necessary modules
 from flask import Flask, render_template, url_for
 import mysql.connector
 import database
 
+# Create a new Flask web server
 app = Flask(__name__)
 
-
+# Define the route for the URL '/'
 @app.route("/")
 def index():
+  # When the route is accessed, render the 'home.html' template
   return render_template('home.html')
 
-
+# Define the route for the URL '/vare'
 @app.route("/vare")
 def vare():
+  # Execute the SQL query 'SELECT * FROM gruppe3_nemlig.Vare' and store the result in 'data'
   data = database.execute_query('SELECT * FROM gruppe3_nemlig.Vare')
+  # Print the data to the console
+  print(data)
+  # When the route is accessed, render the 'vare.html' template and pass the 'data' variable to it
   return render_template('vare.html', data=data)
 
-
+# If this script is run directly (instead of being imported), start the web server
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
